320b72458b7aa7a1821c49f5e7fbffeeb9cfe523
unknown <karankartikey72@gmail.com>
2021-09-18 23:28:09 +0530

    views and styles

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
-const express=require ('express')
-const app=express
-const path=require  ('path')
-const router=express.Router()
-const port=3000
+const express = require("express");
+const app = express();
 
-router.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'views/index.html'));
-    //__dirname : It will resolve to your project folder.
-  });
-  
-  router.get('/about',function(req,res){
-    res.sendFile(path.join(__dirname+'/about.html'));
-  });
-  
-  router.get('/sitemap',function(req,res){
-    res.sendFile(path.join(__dirname+'/sitemap.html'));
-  });
-  
-  //add the router
-  app.use('/', router);
-  app.listen(process.env.port || 3000);
-  
-  console.log('Running at Port 3000');
+const path = require("path");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(__dirname + "/public/login.html");
+});
+
+app.get("/success", (req, res) => {
+  res.sendFile(__dirname + "/public/success.html");
+});
+
+app.get("/pictionary", (req, res) => {
+  res.sendFile(__dirname + "/public/options.html");
+});
+app.get("/pictionary", (req, res) => {
+  res.sendFile(__dirname + "/public/phrases.html");
+});
+app.get("/pictionary", (req, res) => {
+  res.sendFile(__dirname + "/public/pictionary.html");
+});
+
+app.listen("3000", console.log("Listening on port 3000."));
